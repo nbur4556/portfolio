91ef0a3f0827ea05474641d507c0389ada124320
nbur4556 <nbur4556@gmail.com>
2022-03-23 18:32:48 -0500

    feat: redundant alt tags on mobile image

--- components/MobileImage.jsx
+++ components/MobileImage.jsx
@@ -2,6 +2,6 @@ import styles from '../styles/MobileImage.module.scss';
 
 export const MobileImage = ({ imageUrl, altText = '' }) => {
     return <div className={styles.imageContainer}>
-        <span role="img" aria-label={altText} style={{ backgroundImage: `url(${imageUrl})` }} />
+        <span role="img" aria-label={altText} style={{ backgroundImage: `url(${imageUrl})` }}>{altText}</span>
     </div>
 }
