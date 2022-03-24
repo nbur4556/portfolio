0a5dd7a8531b9997efcc7478df908f355b70a907
nbur4556 <nbur4556@gmail.com>
2022-03-23 20:06:10 -0500

    style: static background

--- pages/portfolio/index.jsx
+++ pages/portfolio/index.jsx
@@ -11,6 +11,15 @@ const Portfolio = () => {
         >
             <p>Dev issue tracker is a resource for organizing bugs, issues, and features on development projects. The application features robust state management implementing React&apos;s Context API, making state easily accessible across the various components of the project.</p>
         </PortfolioPost>
+
+        <PortfolioPost title="Dev Issue Tracker"
+            techList={['React', 'Sass', 'Node', 'Express', 'Mongoose', 'JWT']}
+            websiteLink="https://devissuetracking.herokuapp.com/"
+            repoLink="https://github.com/nbur4556/issue_reporter"
+            image={{ src: "/mountain_bg.png", txt: "Mountain background temporary" }}
+        >
+            <p>Dev issue tracker is a resource for organizing bugs, issues, and features on development projects. The application features robust state management implementing React&apos;s Context API, making state easily accessible across the various components of the project.</p>
+        </PortfolioPost>
     </main>
 }
 
